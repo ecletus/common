25d489ce47acd9a2e530079b666c2950be712b24
Moises P. Sena <moisespsena@gmail.com>
2018-08-16 14:36:05 -0300

    Change jinzhu/gorm to moisespsena-go/aorm

--- user.go
+++ user.go
@@ -1,7 +1,7 @@
 package common
 
 import (
-	"github.com/jinzhu/gorm"
+	"github.com/moisespsena-go/aorm"
 )
 
 // CurrentUser is an interface, which is used for qor admin to get current logged user
@@ -13,18 +13,18 @@ type User interface {
 
 var DB_USER = PREFIX + ".user"
 
-func SetUserToDB(db *gorm.DB, user User) *gorm.DB {
+func SetUserToDB(db *aorm.DB, user User) *aorm.DB {
 	return db.Set(DB_USER, user)
 }
 
-func GetUserFromDB(db *gorm.DB) User {
+func GetUserFromDB(db *aorm.DB) User {
 	if v, ok := db.Get(DB_USER); ok {
 		return v.(User)
 	}
 	return nil
 }
 
-func GetUserFromScope(scope *gorm.Scope) User {
+func GetUserFromScope(scope *aorm.Scope) User {
 	if v, ok := scope.Get(DB_USER); ok {
 		return v.(User)
 	}
